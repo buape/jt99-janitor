397b4fde9804e0cb6a380f6c572ece2c1fd83896
Shadow <hi@shadowing.dev>
2022-11-25 23:31:31 -0600

    oops

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,5 +2,6 @@ import bots from "./tags/bots"
 import whatisthis from "./tags/whatisthis"
 import english from "./tags/english"
 import basestation from "./tags/basestation"
+import support from "./tags/support"
 
-export default [whatisthis, bots, english, basestation]
+export default [whatisthis, bots, english, basestation, support]
