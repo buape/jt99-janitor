a7a40db82fa9bb4fcf39c17a7c820637b8f3b439
Shadow <hi@shadowing.dev>
2022-11-25 20:06:31 -0600

    base station

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import bots from "./tags/bots"
 import whatisthis from "./tags/whatisthis"
 import english from "./tags/english"
+import basestation from "./tags/basestation"
 
-export default [whatisthis, bots, english]
+export default [whatisthis, bots, english, basestation]
