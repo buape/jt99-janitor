f743e5af3d97d4cf6f72acbfac459d3ac324ef58
BeanJr_YT <kyle95wm@gmail.com>
2023-10-28 22:22:26 -0400

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,5 +3,6 @@ import whatisthis from "./tags/whatisthis"
 import english from "./tags/english"
 import basestation from "./tags/basestation"
 import support from "./tags/support"
+import getserverinfo from "./tags/getserverinfo"
 
 export default [whatisthis, bots, english, basestation, support]
